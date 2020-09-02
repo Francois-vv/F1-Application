7e89b1534179baff2d16e512ae00a8ff063eb76f
Francois Janse van Vuren <c3185723@uon.edu.au>
2020-09-02 14:48:45 +1000

    Added Navigation and NavigationMenu Components
    The side menu now navigates to different pages

--- f1-app/src/App.js
+++ f1-app/src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import F1Schedule from './Components/F1Schedule'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Views/Home";
+import About from "./Views/About";
 
 function App() {
   return (
     <div>
-      <Header />
-      <F1Schedule />
-      <Footer />
+      <Router>
+        <Header />
+        <div className="p-3">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ f1-app/src/Views/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import F1Schedule from "../Components/F1Schedule";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">This is the home page</h1>
+      <F1Schedule />
+    </div>
+  );
+}
+
+export default Home;
