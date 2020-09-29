8bd2ed82564627d884857bd46c2ccce5b88aa80d
Francois <francois.vv@tutanota.com>
2020-09-29 13:37:19 +1000

    Added Content to About Screen

--- f1-app/src/Views/About.js
+++ f1-app/src/Views/About.js
@@ -3,8 +3,37 @@ import React from 'react'
 function About() {
     return (
         <div className="h-auto">
-            <h1 className="mb-3 text-2xl font-bold text-center">About Us</h1>
-            <p>This is the about page content.</p>
+            <h3 className="pb-3 text-2xl font-bold text-center">
+                Development Information
+            </h3>
+            <div className="flex p-4 bg-gray-700 rounded-md">
+                <ul className="list-inside">
+                    <li className="pb-2">
+                        This application is being developed to display the
+                        current F1 schedule, race results and driver standings.
+                    </li>
+                    <li className="pb-2">
+                        Additional features may be added as development
+                        continues.
+                    </li>
+                    <li className="pb-2">
+                        Please note that this project is for learning purposes
+                        only.
+                    </li>
+                    <li className="pb-2">
+                        Using the Ergast Developer API as the data source.
+                    </li>
+                    <li className="pb-2">
+                        URL:{' '}
+                        <a
+                            className="text-blue-600 underline "
+                            href="https://ergast.com/mrd/"
+                        >
+                            https://ergast.com/mrd/
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
